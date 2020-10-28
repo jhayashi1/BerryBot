4ebfdbef5b60c427ab8f62620723d52dacfc7ecd
BuildTools <unconfigured@null.spigotmc.org>
2020-10-28 17:27:40 -0500

    Implemented functionality for sentry as well as handled permissions

--- storage.py
+++ storage.py
@@ -5,53 +5,53 @@ replist = {}
 
 def handle(name, guild, action, varname=None, data=None):
     if action == "add":
-        o = checkInfo(name)
+        o = check_info(name)
         if o == "Error":
-            addEntry(name, guild)
-            updateInfo(name, varname, data)
+            add_entry(name, guild)
+            update_info(name, varname, data)
         else:
-            updateInfo(name, varname, data)
+            update_info(name, varname, data)
     elif action == "check":
-        o = checkInfo(name)
+        o = check_info(name)
         if o == "Error":
             return "User doesnt exist in database"
         else:
             return o
 
 
-def savetojson(dict):
+def save_to_json(dict):
     with open('varStorage.json', 'w') as fp:
         json.dump(dict, fp, indent=4)
 
 
-def loadfromjson():
+def load_from_json():
     try:
         with open('varStorage.json') as json_file:
             global replist
             replist = json.load(json_file)
     except json.decoder.JSONDecodeError:
-        addEntry("Placeholder", 0, 0)
+        add_entry("Placeholder", 0, 0)
 
 
-def updateInfo(name, varN, data):
-    loadfromjson()
+def update_info(name, varN, data):
+    load_from_json()
     if name in replist and varN is not None and data is not None:
         replist[name][varN] = data
-        savetojson(replist)
+        save_to_json(replist)
     else:
         return "Error"
 
 
 def toggle_info(name, varN):
-    loadfromjson()
+    load_from_json()
     if name in replist and varN in replist.get(name):
         replist[name][varN] = not replist[name][varN]
-        savetojson(replist)
+        save_to_json(replist)
     else:
         return "Error"
 
-def checkInfo(name):
-    loadfromjson()
+def check_info(name):
+    load_from_json()
     try:
         return replist[name]
     except:
@@ -59,18 +59,18 @@ def checkInfo(name):
 
 
 def get_value(name, val):
-    loadfromjson()
+    load_from_json()
     try:
         return replist.get(name, {}).get(val)
     except:
         return "Error"
 
 
-def addEntry(name, id, guild):
+def add_entry(name, id, guild):
     replist[name] = {
         "memberID": id,
         "guildID": guild,
         "sentry_enabled": False
     }
-    savetojson(replist)
+    save_to_json(replist)
     return "done"
