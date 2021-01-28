b4d1a3df733cac6ac412b500f56a81f6cd44c0fd
BuildTools <unconfigured@null.spigotmc.org>
2021-01-27 20:48:39 -0600

    Implemented assemble command and cleaned up

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ import random
 import storage
 import praw
 from discord.ext import commands
+from PyDictionary import PyDictionary
 
+dictionary = PyDictionary()
 token = open("token.txt").read()
 intents = discord.Intents.all()
 intents.members = True
@@ -45,20 +47,20 @@ async def on_ready():
             if storage.check_info(member) == "Error":
                 storage.add_entry(str(member), member.id, member.guild.id)
 
-@bot.event
-async def on_voice_state_update(member, before, after):
-    guild = member.guild
-    event = await guild.audit_logs().find(predicate)
-    name = guild.get_member_named(str(event.user))
-    check = storage.get_value(str(name), 'sentry_enabled')
 
-    if event.user.name != member.name and event.id != bot.last_event.id and check and bot.sentry_on:
-        print("Event: " + event.user.name)
-        print("Member: " + member.name)
-        bot.last_event = event
-        await name.move_to(channel=None)
-        await bot.ch.send(name.mention + " nice try idiot")
-
+# @bot.event
+# async def on_voice_state_update(member, before, after):
+#     guild = member.guild
+#     event = await guild.audit_logs().find(predicate)
+#     name = guild.get_member_named(str(event.user))
+#     check = storage.get_value(str(name), 'sentry_enabled')
+#
+#     if event.user.name != member.name and event.id != bot.last_event.id and check and bot.sentry_on:
+#         print("Event: " + event.user.name)
+#         print("Member: " + member.name)
+#         bot.last_event = event
+#         await name.move_to(channel=None)
+#         await bot.ch.send(name.mention + " nice try idiot")
 
 @bot.command()
 async def refreshdb(ctx):
@@ -90,58 +92,16 @@ async def get(ctx, *args):
         await ctx.send("You have insufficient permissions to access this command")
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send("yeet")
+async def assemble(ctx):
+    home = ctx.author.voice.channel
+    channels = ctx.guild.voice_channels
 
-
-@bot.command()
-async def quote(ctx, arg1):
-    print(ctx.author.name + " quoted " + arg1)
-
-    if arg1.lower() == "vishnu":
-        channel = bot.get_channel(397952381511270400)
-        quote_list = await channel.history().flatten()
-        quote = random.choice(quote_list).content
-        image_link = "https://cdn.discordapp.com/avatars/177592593952014336/187405c7faa6d947b8bc4f016d7e2d51.png?size=128"
-        await ctx.send(embed=send_quote(ctx, "Vishnu", quote, image_link))
-    elif arg1.lower() == "cameron":
-        channel = bot.get_channel(405570948960354346)
-        quote_list = await channel.history().flatten()
-        quote = random.choice(quote_list).content
-        image_link = "https://cdn.discordapp.com/avatars/198587944783577088/d383f2612bcf68348d5afb339af1b584.png?size=128"
-        await ctx.send(embed=send_quote(ctx, "Cameron", quote, image_link))
-    elif arg1.lower() == "jared":
-        channel = bot.get_channel(431654881359822848)
-        quote_list = await channel.history().flatten()
-        quote = random.choice(quote_list).content
-        image_link = "https://cdn.discordapp.com/avatars/187718185779200000/f4f75feca475e8fcd1a86db32f93b3eb.png?size=128"
-        await ctx.send(embed=send_quote(ctx, "Jared", quote, image_link))
-    elif arg1.lower() == "andrew":
-        channel = bot.get_channel(405571631944171527)
-        quote_list = await channel.history().flatten()
-        quote = random.choice(quote_list).content
-        image_link = "https://cdn.discordapp.com/avatars/198297488786980865/cd5c660007a9a17d33498581b7b15ab3.png?size=128"
-        await ctx.send(embed=send_quote(ctx, "Andrew", quote, image_link))
-    elif arg1.lower() == "max":
-        channel = bot.get_channel(405573706774216714)
-        quote_list = await channel.history().flatten()
-        quote = random.choice(quote_list).content
-        image_link = "https://cdn.discordapp.com/avatars/217467339669635073/f66ee4b572d39bb697c7e4b9406c58ca.png?size=128"
-        await ctx.send(embed=send_quote(ctx, "Max", quote, image_link))
-    elif arg1.lower() == "jon":
-        channel = bot.get_channel(498996904710111232)
-        quote_list = await channel.history().flatten()
-        quote = random.choice(quote_list).content
-        image_link = "https://cdn.discordapp.com/avatars/249716884876820481/609225e2e6d8581885e0931d6b9f61e2.png?size=128"
-        await ctx.send(embed=send_quote(ctx, "Jon", quote, image_link))
-    elif arg1.lower() == "gay":
-        channel = bot.get_channel(405573202933317632)
-        quote_list = await channel.history().flatten()
-        quote = random.choice(quote_list).content
-        image_link = "https://cdn.discordapp.com/avatars/198324497655398402/776e76df7dda87a86666665256dda05b.png?size=128"
-        await ctx.send(embed=send_quote(ctx, "Gay Person", quote, image_link))
+    if home is not None:
+        for channel in channels:
+            for member in channel.members:
+                await member.move_to(home)
     else:
-        await ctx.send("Target not recognized")
+        await ctx.send("You're not in a channel!")
 
 
 @bot.command()
@@ -156,6 +116,7 @@ async def sentry(ctx):
     else:
         await ctx.send("You have insufficient permissions to access this command")
 
+
 @bot.command()
 async def watch(ctx, *args):
     if not validate_user(ctx.author):
@@ -164,7 +125,8 @@ async def watch(ctx, *args):
             target = await converter.convert(ctx=ctx, argument=' '.join(args))
             print(str(target))
             storage.toggle_info(str(target), 'sentry_enabled')
-            await ctx.send("Toggled sentry to " + str(storage.get_value(str(target), 'sentry_enabled')) + " for " + str(target))
+            await ctx.send(
+                "Toggled sentry to " + str(storage.get_value(str(target), 'sentry_enabled')) + " for " + str(target))
         except:
             await ctx.send("Invalid User!")
     else:
@@ -174,9 +136,9 @@ async def watch(ctx, *args):
 @bot.command()
 async def reddit(ctx, arg1):
     try:
-        r = praw.Reddit(client_id='',
-                        client_secret='',
-                        user_agent='')
+        r = praw.Reddit(client_id='HrUKB0WP3a6A6Q',
+                        client_secret='rca8u8gHsBLR8xbmho-5FysK8i0cwA',
+                        user_agent='Discord bot')
 
         user = r.redditor(arg1)
         submissions = list(user.submissions.new())
@@ -185,10 +147,10 @@ async def reddit(ctx, arg1):
 
         embed = discord.Embed(
             title=str(submission.title + " from r/" + str(submission.subreddit)),
-            colour=discord.Colour.blue(),
+            colour=discord.Colour.orange(),
             description="`" + url + "`"
         )
-        if submission.is_self or "youtube.com" in submission.selftext :
+        if submission.is_self or "youtube.com" in submission.selftext:
             embed.add_field(name="**Submission Text:**", value=submission.selftext, inline=False)
         else:
             embed.set_image(url=url)
@@ -213,6 +175,24 @@ async def connect(ctx):
 
 
 @bot.command()
+async def sam(ctx):
+    lines = open("adjectives.txt").readlines()
+    meaning = None
+
+    while meaning is None:
+        word = random.choice(lines)
+        meaning = dictionary.meaning(word).get('Adjective')[0]
+
+    embed = discord.Embed(
+        title=word,
+        colour=discord.Colour.orange(),
+        description=dictionary.meaning(word).get('Adjective')[0])
+    await ctx.send("Sam is very " + word)
+    await ctx.send(embed=embed)
+    await ctx.message.delete()
+
+
+@bot.command()
 async def disconnect(ctx):
     await ctx.voice_client.disconnect()
 
@@ -220,5 +200,6 @@ async def disconnect(ctx):
 @bot.command()
 async def close(ctx):
     await bot.close()
+
 
 bot.run(token)
