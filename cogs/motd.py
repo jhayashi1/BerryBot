617d90a839b19779a4ecf29384edceebb6220e59
JaredHayashi <jhayashi@iastate.edu>
2021-10-05 00:07:36 -0500

    users can now be added to motd channel

--- cogs/motd.py
+++ cogs/motd.py
@@ -1,5 +1,7 @@
+import configparser
 from discord.ext import commands
 from utils import getUserByNameOrID
+from configparser import ConfigParser
 import discord
 
 class MotdCog(commands.Cog):
@@ -17,20 +19,33 @@ class MotdCog(commands.Cog):
         target = await getUserByNameOrID(ctx, args)
 
         if target is not None:
+            await addSubscriber(ctx, target)
             response = "User " + target.name +" added to subscription list"
         else:
             try:
-                target = discord.utils.get(discord.guild.roles, name=args)
+                #TODO Make it able to add roles to motd
+                #target = discord.utils.get(ctx.guild.roles, name=' '.join(args))
                 response = "Role " + target.name + " added to subscription list"
             except Exception as e:
                 print(e)
                 response = "No user or role found!"
         await ctx.send(response)
+        await ctx.message.delete()
 
+    @commands.command(brief='Add role or user to subscription list')
+    async def say(self, ctx, args):
+        await ctx.send(args)
+    
+async def addSubscriber(ctx, target):
+        path = "./tools/" + ctx.guild.name + " (" + str(ctx.guild.id) + ")/config.ini"
 
-def addSubscriber(target):
-    return 0
-    #TODO add subscription
+        configur = ConfigParser()
+        configur.read(path)
+        channelId = configur.getint('params', 'motd')
+
+        channel = ctx.bot.get_channel(channelId)
+        await channel.set_permissions(target, read_messages=True, send_messages=False)
+
 
 def removeSubscriber(target):
     return 0
@@ -40,9 +55,12 @@ def listSubscribers():
     return 0
     #TODO list subscribers for channel
 
-def setMotdChannel(channel):
-    return 0
-    #TODO set channel and save to text file
+def setMotdChannel(ctx, channel):
+    path = "./tools/" + ctx.guild.name + " (" + str(ctx.guild.id) + ")/config.ini"
+
+    configur = ConfigParser()
+    configur.read(path)
+    #TODO set channel and save to ini file
 
 def setup(bot):
     bot.add_cog(MotdCog(bot))
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from discord.utils import get
 
 async def getUserByNameOrID(ctx, target):
     #Attempt to search by id
+    #TODO This might not work
     member = ctx.bot.get_user(target)
     if member is not None:
         return member
@@ -10,16 +11,16 @@ async def getUserByNameOrID(ctx, target):
     #Attempt to search by name
     #TODO search by name and discriminator
     name = ' '.join(target)
-    name.split('#')
+    print(name)
     length = len(name)
     converter = commands.MemberConverter()
     try:
         #name + discriminator
         if length > 1 and len(name[1]) == 4 and name[1].isdecimal():
-            get(ctx.bot.get_all_members(), name=name[0], discriminator=name[1])
+            member = get(ctx.bot.get_all_members(), name=name[0], discriminator=name[1])
         #nickname
         else:
-            member = await converter.convert(ctx=ctx, argument=' '.join(str))
+            member = await converter.convert(ctx=ctx, argument=''.join(name))
     except:
         member = None
     return member
